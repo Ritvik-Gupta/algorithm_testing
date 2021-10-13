786b005182f600e5b38eb42e3dc57916b05d84f0
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-10-13 14:52:51 +0530

    Better approach with single HashMap as frequency record and decreasing with other vector

--- src/leetcode/intersection_of_two_arrays_2.rs
+++ src/leetcode/intersection_of_two_arrays_2.rs
@@ -4,22 +4,20 @@ impl Solution {
     pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
         use std::collections::HashMap;
 
-        let mut nums1_record = HashMap::<i32, usize>::new();
+        let mut nums_freq = HashMap::<i32, usize>::new();
         for num in nums1 {
-            *nums1_record.entry(num).or_insert(0) += 1;
-        }
-
-        let mut nums2_record = HashMap::<i32, usize>::new();
-        for num in nums2 {
-            *nums2_record.entry(num).or_insert(0) += 1;
+            *nums_freq.entry(num).or_insert(0) += 1;
         }
 
         let mut result = Vec::new();
-        nums1_record.into_iter().for_each(|(num, count1)| {
-            if let Some(&count2) = nums2_record.get(&num) {
-                result.extend(std::iter::repeat(num).take(std::cmp::min(count1, count2)));
-            }
-        });
+        for num in nums2 {
+            nums_freq.entry(num).and_modify(|freq| {
+                if *freq > 0 {
+                    result.push(num);
+                    *freq -= 1;
+                }
+            });
+        }
 
         result
     }
