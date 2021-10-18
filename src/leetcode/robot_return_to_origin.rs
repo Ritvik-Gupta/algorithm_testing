85e9ecb035ff09b72b79018f297a93b88e8488e4
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-10-18 13:31:07 +0530

    Implementation of "Robot return to Origin" leetcode problem using location pointers

---
+++ src/leetcode/robot_return_to_origin.rs
@@ -0,0 +1,17 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let (mut x, mut y) = (0, 0);
+        for token in moves.chars() {
+            match token {
+                'U' => y += 1,
+                'D' => y -= 1,
+                'L' => x -= 1,
+                'R' => x += 1,
+                _ => panic!(),
+            }
+        }
+        x == 0 && y == 0
+    }
+}
