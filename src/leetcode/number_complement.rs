437a3d2b85aada53d9ac40ca65b007c61af5eded
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-10-25 06:24:53 +0530

    "Number Complement" and "License Key Formatting" leetcode problems

---
+++ src/leetcode/number_complement.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    fn closest_power_of_2(mut num: u32) -> u32 {
+        num |= num >> 1;
+        num |= num >> 2;
+        num |= num >> 4;
+        num |= num >> 8;
+        num |= num >> 16;
+
+        num += 1;
+        return num >> 1;
+    }
+
+    pub fn find_complement(num: i32) -> i32 {
+        let num = num as u32;
+        let all_ones = (Solution::closest_power_of_2(num) << 1) - 1;
+
+        (!num & all_ones) as i32
+    }
+}
