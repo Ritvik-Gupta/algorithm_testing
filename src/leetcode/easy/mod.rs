3419d4a9abe14e0af1f7a112c74fe3e5b0c25926
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-10-29 09:55:33 +0530

    "Determine Color of a Chessboard Square" leetcode easy problem using Odd-Even Pair similarity of Squares

--- src/leetcode/easy/mod.rs
+++ src/leetcode/easy/mod.rs
@@ -2,6 +2,7 @@ pub mod add_binary;
 pub mod add_to_array_form_of_integer;
 pub mod contains_duplicate_2;
 pub mod convert_binary_number_in_a_linked_list_to_integer;
+pub mod determine_color_of_a_chessboard_square;
 pub mod fizz_buzz;
 pub mod happy_number;
 pub mod implement_stack_using_queues;
