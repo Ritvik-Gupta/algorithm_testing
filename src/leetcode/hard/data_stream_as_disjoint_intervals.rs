ae1f2b788d7cff2ca786c3dd4d5a6217e25ac308
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-11-08 20:37:24 +0530

    "Data Stream as Disjoint Intervals" leetcode hard algorithmic problem using Custom Algorithmic Sorted B-Tree Set as sorting for Intersection and Separation for 2 range summaries and checking for lower/upper bounds for Merging 2 or more intervals

--- src/leetcode/hard/data_stream_as_disjoint_intervals.rs
+++ src/leetcode/hard/data_stream_as_disjoint_intervals.rs
@@ -1,34 +1,75 @@
-#[derive(PartialOrd, Ord)]
+#[derive(Debug)]
 struct RangeSummary(i32, i32);
+
+impl RangeSummary {
+    fn contains(&self, val: i32) -> bool {
+        self.0 <= val && val <= self.1
+    }
+}
+
+impl Into<Vec<i32>> for &RangeSummary {
+    fn into(self) -> Vec<i32> {
+        vec![self.0, self.1]
+    }
+}
 
 impl PartialEq for RangeSummary {
     fn eq(&self, other: &Self) -> bool {
-        self.0 < other.0 && other.1 < self.1
+        other.contains(self.0) || other.contains(self.1)
     }
 }
 
 impl Eq for RangeSummary {}
 
+use std::cmp::Ordering::{self, Equal, Greater, Less};
+
+impl PartialOrd for RangeSummary {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self == other {
+            Some(Equal)
+        } else if self.0 > other.1 {
+            Some(Greater)
+        } else if other.0 > self.1 {
+            Some(Less)
+        } else {
+            None
+        }
+    }
+}
+
+impl Ord for RangeSummary {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
 use std::collections::BTreeSet;
 
-#[allow(dead_code)]
 struct SummaryRanges(BTreeSet<RangeSummary>);
 
 impl SummaryRanges {
     #[allow(dead_code)]
     fn new() -> Self {
-        let mut a = BTreeSet::new();
-        a.insert(RangeSummary(1, 1));
-        panic!();
+        Self(BTreeSet::new())
     }
 
     #[allow(dead_code)]
-    fn add_num(&self, _val: i32) {
-        panic!();
+    fn add_num(&mut self, val: i32) {
+        let (mut lower_bound, mut upper_bound) = (val, val);
+
+        if !self.0.contains(&RangeSummary(lower_bound, upper_bound)) {
+            if let Some(lower_range) = self.0.take(&RangeSummary(lower_bound - 1, upper_bound)) {
+                lower_bound = lower_range.0;
+            }
+            if let Some(upper_range) = self.0.take(&RangeSummary(lower_bound, upper_bound + 1)) {
+                upper_bound = upper_range.1;
+            }
+            self.0.insert(RangeSummary(lower_bound, upper_bound));
+        }
     }
 
     #[allow(dead_code)]
     fn get_intervals(&self) -> Vec<Vec<i32>> {
-        panic!();
+        self.0.iter().map(Into::into).collect()
     }
 }
