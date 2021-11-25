dc04af25706d44db14b2fd129fd5909531917345
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2021-11-25 06:18:45 +0530

    "Find K Closest Elements" leetcode medium problem using Binary Search for Beginning of Window and comparing with End of Window according to Distance from X

--- src/leetcode/medium/find_k_closest_elements.rs
+++ src/leetcode/medium/find_k_closest_elements.rs
@@ -1,21 +1,18 @@
 pub struct Solution;
 
-const IMPOSSIBLE_ARR_ELEMENT: i32 = 20001;
-
 impl Solution {
     pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
-        let abs_factor = |pos| match arr.get(pos) {
-            Some(elm) => (elm - x).abs(),
-            None => IMPOSSIBLE_ARR_ELEMENT,
-        };
+        let window_size = k as usize;
 
-        let smallest_elm_pos = (0..arr.len()).min_by_key(|&pos| abs_factor(pos)).unwrap();
-        let (mut lower, mut upper) = (smallest_elm_pos, smallest_elm_pos);
+        let (mut left_idx, mut right_idx) = (0, arr.len() - window_size);
+        while left_idx < right_idx {
+            let mid_idx = (left_idx + right_idx) / 2;
+            match x - arr[mid_idx] > arr[mid_idx + window_size] - x {
+                true => left_idx = mid_idx + 1,
+                false => right_idx = mid_idx,
+            }
+        }
 
-        (1..k).for_each(|_| match abs_factor(lower - 1) <= abs_factor(upper + 1) {
-            true => lower -= 1,
-            false => upper += 1,
-        });
-        arr[lower..=upper].into()
+        arr[left_idx..left_idx + window_size].into()
     }
 }
