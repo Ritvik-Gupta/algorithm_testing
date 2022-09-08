ed47c55a254a45db1d44105d9013e639687749c3
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-09-08 16:31:44 +0530

    path sum optimizations

--- src/leetcode/medium/path_sum_3.rs
+++ src/leetcode/medium/path_sum_3.rs
@@ -4,63 +4,46 @@ crate::leetcode::binary_tree_definition!();
 use std::cell::RefCell;
 use std::rc::Rc;
 
-type Link<T> = Rc<RefCell<T>>;
+type LinkNode = Rc<RefCell<TreeNode>>;
 
-struct PathSumStore {
+fn find_path_sum(
+    root: &Option<LinkNode>,
     target_sum: i32,
-    total_count: i32,
-    parent_path_sums: Vec<i32>,
-}
+    parent_path_sums: &mut Vec<i128>,
+) -> i32 {
+    if root.is_none() {
+        return 0;
+    }
+    let root = root.as_ref().unwrap();
 
-impl PathSumStore {
-    fn new(target_sum: i32) -> Self {
-        PathSumStore {
-            target_sum,
-            total_count: 0,
-            parent_path_sums: Vec::new(),
-        }
+    let mut total_count = 0;
+
+    let root_borrow = root.borrow();
+    if root_borrow.val == target_sum {
+        total_count += 1;
     }
 
-    fn find_path_sum(&mut self, root: Link<TreeNode>) {
-        let root_borrow = root.borrow();
-
-        for path_sum in self.parent_path_sums.iter_mut() {
-            *path_sum = *path_sum + root_borrow.val;
-            if *path_sum == self.target_sum {
-                self.total_count += 1;
-            }
+    parent_path_sums.iter_mut().for_each(|path_sum| {
+        *path_sum = *path_sum + root_borrow.val as i128;
+        if *path_sum == target_sum as i128 {
+            total_count += 1;
         }
+    });
+    parent_path_sums.push(root_borrow.val as i128);
 
-        self.parent_path_sums.push(root_borrow.val);
-        if root_borrow.val == self.target_sum {
-            self.total_count += 1;
-        }
+    total_count += find_path_sum(&root_borrow.left, target_sum, parent_path_sums);
+    total_count += find_path_sum(&root_borrow.right, target_sum, parent_path_sums);
 
-        root_borrow
-            .left
-            .clone()
-            .map(|left| self.find_path_sum(left));
+    parent_path_sums.pop();
+    parent_path_sums
+        .iter_mut()
+        .for_each(|path_sum| *path_sum = *path_sum - root_borrow.val as i128);
 
-        root_borrow
-            .right
-            .clone()
-            .map(|right| self.find_path_sum(right));
-
-        self.parent_path_sums.pop();
-
-        self.parent_path_sums
-            .iter_mut()
-            .for_each(|path_sum| *path_sum = *path_sum - root_borrow.val);
-    }
+    total_count
 }
 
 impl Solution {
-    pub fn path_sum(root: Option<Link<TreeNode>>, target_sum: i32) -> i32 {
-        root.map(|root| {
-            let mut path_sum_store = PathSumStore::new(target_sum);
-            path_sum_store.find_path_sum(root);
-            path_sum_store.total_count
-        })
-        .unwrap_or(0)
+    pub fn path_sum(root: Option<LinkNode>, target_sum: i32) -> i32 {
+        find_path_sum(&root, target_sum, &mut Vec::new())
     }
 }
