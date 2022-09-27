f0b1e10079375249d0723f2839c13e235c19fb64
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-09-27 11:21:27 +0530

    potd

--- src/leetcode/medium/satisfiability_of_equality_equations.rs
+++ src/leetcode/medium/satisfiability_of_equality_equations.rs
@@ -1,69 +1,62 @@
 crate::leetcode::solution!();
 
-struct Equation {
-    left_var: char,
-    are_equal: bool,
-    right_var: char,
+fn letter_transpose(letter: char) -> usize {
+    (letter as u8 - b'a') as usize
 }
 
-impl Equation {
-    fn from(eqn_string: &String) -> Self {
-        let mut eqn = eqn_string.chars();
-        let (left_var, are_equal) = (eqn.next().unwrap(), eqn.next().unwrap() == '=');
-        eqn.next();
+struct EqnUnionFind {
+    root: Vec<usize>,
+}
+
+impl EqnUnionFind {
+    fn new() -> Self {
         Self {
-            left_var,
-            are_equal,
-            right_var: eqn.next().unwrap(),
+            root: (0..26).collect(),
         }
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let root_x = self.find_root(x);
+        let root_y = self.find_root(y);
+
+        if root_x == root_y {
+            return;
+        }
+        self.root[root_y] = root_x;
+    }
+
+    fn find_root(&mut self, token: usize) -> usize {
+        if token != self.root[token] {
+            self.root[token] = self.find_root(self.root[token]);
+        }
+        self.root[token]
     }
 }
 
 impl Solution {
     pub fn equations_possible(equations: Vec<String>) -> bool {
-        use std::collections::BTreeMap;
+        let mut eqn_clusters = EqnUnionFind::new();
+        let mut disjoint_tokens = Vec::new();
 
-        let (mut variable_record, mut incremental_value) = (BTreeMap::new(), 0);
+        for eqn in equations.iter() {
+            let mut eqn = eqn.chars();
 
-        for equation in equations
-            .iter()
-            .map(Equation::from)
-            .filter(|eqn| eqn.are_equal)
-        {
-            if !variable_record.contains_key(&equation.left_var) {
-                variable_record.insert(equation.left_var, incremental_value);
-                incremental_value += 1;
-            }
+            let token_x = letter_transpose(eqn.next().unwrap());
+            let is_equal_symbol = eqn.next().unwrap() == '=';
+            eqn.next().unwrap();
+            let token_y = letter_transpose(eqn.next().unwrap());
 
-            if !variable_record.contains_key(&equation.right_var) {
-                variable_record.insert(equation.right_var, variable_record[&equation.left_var]);
-            }
-
-            if variable_record[&equation.right_var] != variable_record[&equation.left_var] {
+            if is_equal_symbol {
+                eqn_clusters.union(token_x, token_y);
+            } else if eqn_clusters.find_root(token_x) == eqn_clusters.find_root(token_y) {
                 return false;
+            } else {
+                disjoint_tokens.push((token_x, token_y));
             }
         }
 
-        for equation in equations
-            .iter()
-            .map(Equation::from)
-            .filter(|eqn| !eqn.are_equal)
-        {
-            if !variable_record.contains_key(&equation.left_var) {
-                variable_record.insert(equation.left_var, incremental_value);
-                incremental_value += 1;
-            }
-
-            if !variable_record.contains_key(&equation.right_var) {
-                variable_record.insert(equation.right_var, incremental_value);
-                incremental_value += 1;
-            }
-
-            if variable_record[&equation.right_var] == variable_record[&equation.left_var] {
-                return false;
-            }
-        }
-
-        true
+        disjoint_tokens.iter().all(|&(token_x, token_y)| {
+            eqn_clusters.find_root(token_x) != eqn_clusters.find_root(token_y)
+        })
     }
 }
---
+++ src/leetcode/medium/sender_with_largest_word_count.rs
@@ -0,0 +1,32 @@
+crate::leetcode::solution!();
+
+use std::cmp::Ordering::*;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn largest_word_count(messages: Vec<String>, senders: Vec<String>) -> String {
+        let mut word_count_table = HashMap::with_capacity(senders.len());
+        word_count_table.insert("".to_string(), 0);
+
+        messages
+            .into_iter()
+            .zip(senders.into_iter())
+            .for_each(|(msg, sender)| {
+                *word_count_table.entry(sender).or_insert(0) += msg.split(' ').count();
+            });
+
+        let mut largest_count_sender = "".to_string();
+
+        for (sender, &word_count) in word_count_table.iter() {
+            let sender = sender.clone();
+
+            match word_count_table[&largest_count_sender].cmp(&word_count) {
+                Less => largest_count_sender = sender,
+                Equal if largest_count_sender < sender => largest_count_sender = sender,
+                _ => {}
+            }
+        }
+
+        largest_count_sender
+    }
+}
