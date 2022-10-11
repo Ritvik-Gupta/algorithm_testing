3beddee62f286d10a566ab7a2998b16224e1a001
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-10-11 11:00:01 +0530

    potd

--- leetcode/src/medium/increasing_triplet_subsequence.rs
+++ leetcode/src/medium/increasing_triplet_subsequence.rs
@@ -1,42 +1,22 @@
 crate::solution!();
 
-struct Triplet {
-    triplet: [i32; 3],
-    size: usize,
-}
-
-impl Triplet {
-    fn new() -> Self {
-        Self {
-            triplet: [i32::MAX; 3],
-            size: 0,
-        }
-    }
-
-    fn search_add(&mut self, num: i32) {
-        if let Some(found_idx) = self.triplet[0..self.size]
-            .iter()
-            .position(|&val| val >= num)
-        {
-            self.triplet[found_idx] = num;
-        } else {
-            self.triplet[self.size] = num;
-            self.size += 1;
-        }
-    }
-}
-
 impl Solution {
     pub fn increasing_triplet(nums: Vec<i32>) -> bool {
-        let mut triplet = Triplet::new();
+        let mut inc_subseq = Vec::with_capacity(3);
 
-        for &num in nums.iter() {
-            triplet.search_add(num);
-            if triplet.size == 3 {
-                break;
+        nums.iter().any(|&num| {
+            let idx = inc_subseq
+                .iter()
+                .position(|&x| x >= num)
+                .unwrap_or(inc_subseq.len());
+
+            if idx < inc_subseq.len() {
+                inc_subseq[idx] = num;
+            } else {
+                inc_subseq.push(num);
             }
-        }
 
-        triplet.size == 3
+            inc_subseq.len() == 3
+        })
     }
 }
