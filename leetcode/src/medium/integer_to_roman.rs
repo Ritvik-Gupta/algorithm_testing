dddf8c2c40f596f838e6b9da7ba44e301871ae55
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-10-20 10:29:40 +0530

    potd

--- leetcode/src/medium/integer_to_roman.rs
+++ leetcode/src/medium/integer_to_roman.rs
@@ -1,26 +1,25 @@
 crate::solution!();
 
+static ROMAN_NUMERALS: [(i32, &str); 13] = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+];
+
 impl Solution {
     pub fn int_to_roman(mut num: i32) -> String {
-        use std::collections::BTreeMap;
-
-        let mut roman_numerals: BTreeMap<i32, &str> = BTreeMap::new();
-        roman_numerals.insert(1000, "M");
-        roman_numerals.insert(900, "CM");
-        roman_numerals.insert(500, "D");
-        roman_numerals.insert(400, "CD");
-        roman_numerals.insert(100, "C");
-        roman_numerals.insert(90, "XC");
-        roman_numerals.insert(50, "L");
-        roman_numerals.insert(40, "XL");
-        roman_numerals.insert(10, "X");
-        roman_numerals.insert(9, "IX");
-        roman_numerals.insert(5, "V");
-        roman_numerals.insert(4, "IV");
-        roman_numerals.insert(1, "I");
-
-        let mut result = "".to_string();
-        roman_numerals.iter().rev().for_each(|(&value, &symbol)| {
+        let mut result = String::new();
+        ROMAN_NUMERALS.iter().for_each(|&(value, symbol)| {
             while num >= value {
                 result += symbol;
                 num -= value;
