de00421be30ed477a134e7f128b7e21fc16bd09f
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-11-10 13:46:29 +0530

    potd

--- leetcode/src/medium/online_stock_span.rs
+++ leetcode/src/medium/online_stock_span.rs
@@ -1,41 +1,31 @@
-struct StockNode {
-    date: i32,
-    price: i32,
-    next: Option<Box<StockNode>>,
-}
-
-impl StockNode {
-    fn new(date: i32, price: i32, next: Option<Box<Self>>) -> Box<Self> {
-        Box::new(Self { date, price, next })
-    }
-
-    fn get_date(stock: &Option<Box<StockNode>>) -> i32 {
-        stock.as_ref().map(|stock| stock.date).unwrap_or(0)
-    }
-}
-
 struct StockSpanner {
-    stock_list: Option<Box<StockNode>>,
+    stocks: Vec<(i32, i32)>,
 }
 
 impl StockSpanner {
     #[allow(dead_code)]
     fn new() -> Self {
-        Self { stock_list: None }
+        Self { stocks: Vec::new() }
+    }
+
+    #[inline]
+    fn recent_stock_date(&self) -> i32 {
+        self.stocks.last().map(|stock| stock.1).unwrap_or(0)
     }
 
     #[allow(dead_code)]
     fn next(&mut self, price: i32) -> i32 {
-        let current_date = StockNode::get_date(&self.stock_list);
-        let mut curr_stock = self.stock_list.take();
-        loop {
-            match curr_stock {
-                Some(mut stock) if price >= stock.price => curr_stock = stock.next.take(),
-                _ => break,
+        let current_date = self.recent_stock_date();
+
+        while let Some(recent_stock) = self.stocks.last() {
+            if recent_stock.0 > price {
+                break;
             }
+            self.stocks.pop();
         }
-        let prev_stock_date = StockNode::get_date(&curr_stock);
-        self.stock_list = Some(StockNode::new(current_date + 1, price, curr_stock));
-        StockNode::get_date(&self.stock_list) - prev_stock_date
+
+        let prev_stock_date = self.recent_stock_date();
+        self.stocks.push((price, current_date + 1));
+        self.recent_stock_date() - prev_stock_date
     }
 }
