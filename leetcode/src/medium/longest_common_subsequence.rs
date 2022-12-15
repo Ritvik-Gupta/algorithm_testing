2651941c4c13516de7e0e05735522efa169107c5
Ritvik-Gupta <ritvikg9@gmail.com>
2022-12-15 12:39:49 +0530

    leetcode potd improvement

--- leetcode/src/medium/longest_common_subsequence.rs
+++ leetcode/src/medium/longest_common_subsequence.rs
@@ -1,24 +1,19 @@
 crate::solution!();
 
-#[derive(Hash, PartialEq, Eq)]
-struct DpKey(usize, usize);
-
-use std::collections::HashMap;
-
 struct TabulatedSubSeq<'a> {
     text1: &'a [u8],
     text2: &'a [u8],
-    memo: HashMap<DpKey, i32>,
+    memo: Vec<Vec<i32>>,
 }
 
 impl<'a> TabulatedSubSeq<'a> {
     fn new(text1: &'a str, text2: &'a str) -> Self {
-        let mut memo = HashMap::new();
+        let mut memo = vec![vec![-1; text2.len() + 1]; text1.len() + 1];
         for i in 0..text1.len() + 1 {
-            memo.insert(DpKey(i, 0), 0);
+            memo[i][0] = 0;
         }
         for j in 0..text2.len() + 1 {
-            memo.insert(DpKey(0, j), 0);
+            memo[0][j] = 0;
         }
 
         TabulatedSubSeq {
@@ -29,17 +24,15 @@ impl<'a> TabulatedSubSeq<'a> {
     }
 
     fn bottom_up(&mut self, m: usize, n: usize) -> i32 {
-        if let Some(&stored) = self.memo.get(&DpKey(m, n)) {
-            return stored;
+        if self.memo[m][n] != -1 {
+            return self.memo[m][n];
         }
 
-        let store = if self.text1[m - 1] == self.text2[n - 1] {
-            1 + self.bottom_up(m - 1, n - 1)
-        } else {
-            std::cmp::max(self.bottom_up(m - 1, n), self.bottom_up(m, n - 1))
+        self.memo[m][n] = match self.text1[m - 1] == self.text2[n - 1] {
+            true => 1 + self.bottom_up(m - 1, n - 1),
+            _ => i32::max(self.bottom_up(m - 1, n), self.bottom_up(m, n - 1)),
         };
-        self.memo.insert(DpKey(m, n), store);
-        store
+        self.memo[m][n]
     }
 }
 
