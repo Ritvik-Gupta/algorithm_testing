9792d3c8e1cced072d4166a7bde9b94ac549b1ed
Ritvik-Gupta <ritvikg9@gmail.com>
2022-12-17 11:40:54 +0530

    updated leetcode potd method

--- leetcode/src/medium/evaluate_reverse_polish_notation.rs
+++ leetcode/src/medium/evaluate_reverse_polish_notation.rs
@@ -12,15 +12,19 @@ fn apply_operator(opr: &str, num1: i32, num2: i32) -> i32 {
 
 impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
-        let mut num_stack = Vec::new();
-        tokens.iter().for_each(|token| match token.as_str() {
-            "+" | "-" | "*" | "/" => {
-                let (num2, num1) = (num_stack.pop().unwrap(), num_stack.pop().unwrap());
-                num_stack.push(apply_operator(token, num1, num2));
-            }
-            _ => num_stack.push(token.parse().unwrap()),
-        });
-
-        num_stack.pop().unwrap()
+        tokens
+            .iter()
+            .fold(Vec::new(), |mut num_stack, token| {
+                match token.as_str() {
+                    "+" | "-" | "*" | "/" => {
+                        let (num2, num1) = (num_stack.pop().unwrap(), num_stack.pop().unwrap());
+                        num_stack.push(apply_operator(token, num1, num2));
+                    }
+                    _ => num_stack.push(token.parse().unwrap()),
+                };
+                num_stack
+            })
+            .pop()
+            .unwrap()
     }
 }
