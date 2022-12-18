310216723924b1b6e5c82ecde53a8b06ae82601f
Ritvik-Gupta <ritvikg9@gmail.com>
2022-12-18 11:37:12 +0530

    updated leetcode potd solution

--- leetcode/src/medium/daily_temperatures.rs
+++ leetcode/src/medium/daily_temperatures.rs
@@ -1,19 +1,29 @@
 crate::solution!();
 
 impl Solution {
-    pub fn daily_temperatures(mut temperatures: Vec<i32>) -> Vec<i32> {
-        let mut temperature_stack = Vec::with_capacity(temperatures.len());
-        for (pos, temperature) in temperatures.iter_mut().enumerate().rev() {
-            let temp = *temperature;
-            *temperature = loop {
-                match temperature_stack.last() {
-                    Some(&(top_temp, _)) if temp >= top_temp => temperature_stack.pop(),
-                    Some(&(_, temp_idx)) => break (temp_idx - pos) as i32,
-                    None => break 0,
-                };
-            };
-            temperature_stack.push((temp, pos));
-        }
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut stack = Vec::new();
+        let res: *mut i32 = temperatures.as_ptr() as *mut _;
+
+        temperatures
+            .iter()
+            .zip(0..)
+            .for_each(|(&temperature, today)| {
+                loop {
+                    match stack.last() {
+                        Some(&(last_temp, day)) if last_temp < temperature => {
+                            stack.pop();
+                            *unsafe { &mut *res.offset(day) } = (today - day) as i32;
+                        }
+                        _ => break,
+                    }
+                }
+                stack.push((temperature, today));
+            });
+
+        stack
+            .into_iter()
+            .for_each(|(_, day)| *unsafe { &mut *res.offset(day) } = 0);
 
         temperatures
     }
