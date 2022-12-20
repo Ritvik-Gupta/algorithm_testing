c235042b3b01e8bc4f345b896b72d1f848867b22
Ritvik-Gupta <ritvikg9@gmail.com>
2022-12-21 00:13:48 +0530

    minor change

--- advent-of-code/src/aoc_2022.rs
+++ advent-of-code/src/aoc_2022.rs
@@ -1,4 +1,3 @@
-pub mod Pyroclastic_Flow;
 pub mod beacon_exclusion_zone;
 pub mod boiling_boulders;
 pub mod calorie_counting;
@@ -10,6 +9,7 @@ pub mod hill_climbing_algorithm;
 pub mod monkey_in_the_middle;
 pub mod no_space_left_on_device;
 pub mod proboscidea_volcanium;
+pub mod pyroclastic_Flow;
 pub mod regolith_reservoir;
 pub mod rock_paper_scissors;
 pub mod rope_bridge;
