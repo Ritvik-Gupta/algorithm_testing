47d8fcc1e0eecd0b3144b7ed6f18877c720c75c7
Ritvik-Gupta <ritvikg9@gmail.com>
2023-01-12 10:30:44 +0530

    minor change

--- leetcode/src/medium/number_of_nodes_in_the_sub_tree_with_the_same_label.rs
+++ leetcode/src/medium/number_of_nodes_in_the_sub_tree_with_the_same_label.rs
@@ -2,6 +2,7 @@ crate::solution!();
 
 fn recur_subtree_labels(
     node: usize,
+    parent_node: usize,
     adj: &Vec<Vec<usize>>,
     labels: &[u8],
     res: &mut Vec<i32>,
@@ -9,10 +10,13 @@ fn recur_subtree_labels(
     let mut freq_table = [0; 26];
     freq_table[(labels[node] - b'a') as usize] += 1;
 
-    adj[node].iter().for_each(|&child_node| {
-        let child_table = recur_subtree_labels(child_node, adj, labels, res);
-        (0..26).for_each(|i| freq_table[i] += child_table[i]);
-    });
+    adj[node]
+        .iter()
+        .filter(|&&child_node| child_node != parent_node)
+        .for_each(|&child_node| {
+            let child_table = recur_subtree_labels(child_node, node, adj, labels, res);
+            (0..26).for_each(|i| freq_table[i] += child_table[i]);
+        });
 
     res[node] = freq_table[(labels[node] - b'a') as usize];
     freq_table
@@ -26,10 +30,11 @@ impl Solution {
         edges.iter().for_each(|edge| {
             let (a, b) = (edge[0] as usize, edge[1] as usize);
             adj[a].push(b);
+            adj[b].push(a);
         });
 
         let mut res = vec![-1; n];
-        recur_subtree_labels(0, &adj, &labels.as_bytes(), &mut res);
+        recur_subtree_labels(0, usize::MAX, &adj, &labels.as_bytes(), &mut res);
         res
     }
 }
