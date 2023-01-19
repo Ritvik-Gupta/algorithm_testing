3e2047df9f0e8109546157ef1ee830d1d72a2a9f
Ritvik-Gupta <ritvikg9@gmail.com>
2023-01-19 16:30:06 +0530

    minor change

--- leetcode/src/medium/smallest_number_in_infinite_set.rs
+++ leetcode/src/medium/smallest_number_in_infinite_set.rs
@@ -3,10 +3,12 @@ use std::collections::BTreeSet;
 struct SmallestInfiniteSet(BTreeSet<i32>);
 
 impl SmallestInfiniteSet {
+    #[allow(dead_code)]
     fn new() -> Self {
         Self(BTreeSet::from([1]))
     }
 
+    #[allow(dead_code)]
     fn pop_smallest(&mut self) -> i32 {
         let num = *self.0.range(0..).next().unwrap();
         self.0.remove(&num);
@@ -16,6 +18,7 @@ impl SmallestInfiniteSet {
         num
     }
 
+    #[allow(dead_code)]
     fn add_back(&mut self, num: i32) {
         if *self.0.range(0..).next_back().unwrap() <= num {
             return;
