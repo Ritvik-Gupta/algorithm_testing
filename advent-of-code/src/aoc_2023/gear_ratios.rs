2357f0d1fa72419e81261a7d9a19c567ee30678c
Ritvik-Gupta <ritvikg9@gmail.com>
2023-12-22 11:47:55 +0530

    Minor enhancement for AOC Day 3 - Gear Ratios

--- advent-of-code/src/aoc_2023/gear_ratios.rs
+++ advent-of-code/src/aoc_2023/gear_ratios.rs
@@ -10,6 +10,16 @@ pub struct EngineGrid {
     n: usize,
 }
 
+impl EngineGrid {
+    fn iterate(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (0..self.m).flat_map(|i| (0..self.n).map(move |j| (i, j)))
+    }
+
+    fn at(&self, i: usize, j: usize) -> u8 {
+        *self.grid.get(i).and_then(|row| row.get(j)).unwrap_or(&b'.')
+    }
+}
+
 impl crate::AdventDayProblem for GearRatios {
     type Arg = EngineGrid;
 
@@ -31,39 +41,39 @@ impl crate::AdventDayProblem for GearRatios {
     fn part_1(engine: Self::Arg) -> Self::Ret {
         let mut symbol_schematic: Vec<Vec<bool>> = vec![vec![false; engine.n]; engine.m];
 
-        for i in 0..engine.m {
-            for j in 0..engine.n {
-                if is_symbol(engine.grid[i][j]) {
-                    for x in i.wrapping_sub(1)..=usize::min(engine.m, i + 1) {
-                        for y in j.wrapping_sub(1)..=usize::min(engine.n, j + 1) {
-                            symbol_schematic[x][y] = true;
-                        }
-                    }
+        engine.iterate().for_each(|(i, j)| {
+            if !is_symbol(engine.at(i, j)) {
+                return;
+            }
+            for x in i.wrapping_sub(1)..=usize::min(engine.m, i + 1) {
+                for y in j.wrapping_sub(1)..=usize::min(engine.n, j + 1) {
+                    symbol_schematic[x][y] = true;
                 }
             }
-        }
+        });
 
         let mut engine_cumulative = 0;
         let mut current_number = 0;
         let mut is_marked = false;
-        for i in 0..engine.m {
-            for j in 0..engine.n {
-                if engine.grid[i][j].is_ascii_digit() {
-                    current_number = current_number * 10 + (engine.grid[i][j] - b'0') as u32;
-                    is_marked |= symbol_schematic[i][j];
-                } else {
+
+        engine.iterate().for_each(|(i, j)| {
+            if engine.at(i, j).is_ascii_digit() {
+                current_number = current_number * 10 + (engine.at(i, j) - b'0') as u32;
+                is_marked |= symbol_schematic[i][j];
+
+                if !engine.at(i, j + 1).is_ascii_digit() {
                     engine_cumulative += current_number * is_marked as u32;
 
                     current_number = 0;
                     is_marked = false;
                 }
             }
-        }
+        });
 
         engine_cumulative
     }
 
-    fn part_2(arg: Self::Arg) -> Self::Ret {
+    fn part_2(engine: Self::Arg) -> Self::Ret {
         todo!()
     }
 }
